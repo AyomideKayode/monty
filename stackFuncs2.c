c83d3d0f543fc778b65de51386c897e43fe34ed2
Ayomide <ayomidekay7@gmail.com>
2023-09-21 05:27:15 +0100

    Task 5: Do Nothing opcode

--- stackFuncs2.c
+++ stackFuncs2.c
@@ -47,3 +47,14 @@ void swap(stack_t **h, unsigned int line_number)
 	node->next = *h;
 	(*h) = node;
 }
+
+/**
+ * nop - doesn’t do anything.
+ * @h: head of list
+ * @line_number: bytecode line number
+ */
+void nop(stack_t **h, unsigned int line_number)
+{
+	(void)h;
+	(void)line_number;
+}
