3333fa1ab47e22f45c4a27b9dc25e3b27abcb59a
Ayomide <ayomidekay7@gmail.com>
2023-09-21 14:49:19 +0100

    Hot fix: Math operators.

--- math_stackFuncs.c
+++ math_stackFuncs.c
@@ -41,27 +41,6 @@ void _sub(stack_t **h, unsigned int line_number)
 }
 
 /**
- * _mul - multiplies the second top element of the stack
- * with the top element of the stack.
- * @h: top of list
- * @line_number: line of command
- */
-
-void _mul(stack_t **h, unsigned int line_number)
-{
-	if (*h == NULL || (*h)->next == NULL)
-	{
-		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
-		free_list(h);
-		exit(EXIT_FAILURE);
-	}
-	(*h)->next->n *= (*h)->n;
-	(*h) = (*h)->next;
-	free((*h)->prev);
-	(*h)->prev = NULL;
-}
-
-/**
  * _div - divides the second top element of the stack
  * by the top element of the stack.
  * @h: top of list
@@ -78,9 +57,31 @@ void _div(stack_t **h, unsigned int line_number)
 	}
 	if ((*h)->n == 0 || (*h)->next->n == 0)
 	{
-		printf("L%u: division by zero\n", line_number);
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_list(h);
 	}
 	(*h)->next->n /= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
+}
+
+/**
+ * _mul - multiplies the second top element of the stack
+ * with the top element of the stack.
+ * @h: top of list
+ * @line_number: line of command
+ */
+
+void _mul(stack_t **h, unsigned int line_number)
+{
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		free_list(h);
+		exit(EXIT_FAILURE);
+	}
+	(*h)->next->n *= (*h)->n;
 	(*h) = (*h)->next;
 	free((*h)->prev);
 	(*h)->prev = NULL;
@@ -104,7 +105,9 @@ void _mod(stack_t **h, unsigned int line_number)
 	}
 	if ((*h)->n == 0 || (*h)->next->n == 0)
 	{
-		printf("L%u: division by zero\n", line_number);
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_list(h);
+		exit(EXIT_FAILURE);
 	}
 	(*h)->next->n %= (*h)->n;
 	(*h) = (*h)->next;
